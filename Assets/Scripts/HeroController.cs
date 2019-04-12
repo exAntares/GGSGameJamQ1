2010c90facd1e6ebb8c5a46a6c665e5916e321d6
exAntares <luis.andres81@gmail.com>
2019-04-12 23:08:52 +0200

    Added constrains

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 
 public class HeroController : MonoBehaviour {
-    private float Gravity = 8.9f;
+    [SerializeField] private float _gravity = 8.9f;
+    [SerializeField] private Vector2 _minMaxX;
+    [SerializeField] private Vector2 _minMaxY;
+
     private Vector2 Impulse = Vector2.zero;
     private float lastSign = 1;
 
@@ -22,8 +25,9 @@ public class HeroController : MonoBehaviour {
         currentPos += new Vector3(Impulse.x, Impulse.y);
         Impulse *= 0.5f;
 
-        currentPos.y -= Gravity * 0.01f;
-        currentPos.y = currentPos.y <= 0 ? 0 : currentPos.y;
+        currentPos.y -= _gravity * 0.01f;
+        currentPos.y = currentPos.y <= _minMaxY.x ? _minMaxY.x : currentPos.y >= _minMaxY.y ? _minMaxY.y : currentPos.y;
+        currentPos.x = currentPos.x <= _minMaxX.x ? _minMaxX.x : currentPos.x >= _minMaxX.y ? _minMaxX.y : currentPos.x;
         transform.position = currentPos;
     }
 }

846f92cca731014907d5eb4c26b57ef1062a5384
exAntares <luis.andres81@gmail.com>
2019-04-12 20:33:44 +0200

    Added a fast psd import to import everything from a psd into DropboxOutbox

---
+++ Assets/Scripts/Editor/FastPsdImport.cs
@@ -0,0 +1,45 @@
+﻿using SubjectNerd.PsdImporter;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace HalfBlind {
+    public static class FastPsdImport {
+        private const string MENU_ASSET_IMPORT = "Assets/Import PSD Layers (FAST)";
+
+        [MenuItem(MENU_ASSET_IMPORT)]
+        private static void ImportPsdAsset() {
+            Object[] selectionArray = Selection.objects;
+            if (selectionArray.Length < 1) {
+                return;
+            }
+
+            for (int i = 0; i < selectionArray.Length; i++) {
+                var file = selectionArray[i];
+                var path = AssetDatabase.GetAssetPath(file);
+                if (path.ToLower().EndsWith(".psd")) {
+                    ImportPsd(file);
+                    return;
+                }
+            }
+        }
+
+        private static void ImportPsd(Object importFile) {
+            var importSettings = new ImportUserData {
+                fileNaming = NamingConvention.CreateGroupFolders,
+                groupMode = GroupMode.FullPath,
+                TargetDirectory = "Assets/DropboxOutbox"
+            };
+
+            PsdImporter.BuildImportLayerData(importFile, importSettings, (importedLayerData, displayData) => {
+                importSettings.DocRoot = importedLayerData;
+                List<int[]> importLayersList = importSettings.DocRoot.Childs
+                .Select(x => x.indexId)
+                .ToList();
+
+                PsdImporter.ImportLayersUI(importFile, importSettings, importLayersList);
+            });
+        }
+    }
+}

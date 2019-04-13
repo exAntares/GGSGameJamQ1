f41b00c3d7f26657640c161ff3701cf2a57bd92d
exAntares <luis.andres81@gmail.com>
2019-04-13 22:02:05 +0200

    Updating UI, new generic nodes

---
+++ Assets/Scripts/FlowNodes/GetEquipmentData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace HalfBlind.Nodes {
+    [CreateNodeMenu("GameJam/" + nameof(GetEquipmentData))]
+    public class GetEquipmentData : MonoNode {
+        [Input] public ScriptableEquipment Equipment;
+        [Output] public float Health;
+        [Output] public float Damage;
+        [Output] public Sprite Image;
+
+        public override object GetValue(NodePort port) {
+            if (port.fieldName == nameof(Health)) {
+                var input = GetInputValue<ScriptableEquipment>(nameof(Equipment), Equipment);
+                return input?.value.ExtraHealth;
+            }
+
+            if (port.fieldName == nameof(Damage)) {
+                var input = GetInputValue<ScriptableEquipment>(nameof(Equipment), Equipment);
+                return input?.value.ExtraDamage;
+            }
+
+            if (port.fieldName == nameof(Image)) {
+                var input = GetInputValue<ScriptableEquipment>(nameof(Equipment), Equipment);
+                return input?.value.Graphics;
+            }
+
+            return null;
+        }
+    }
+}

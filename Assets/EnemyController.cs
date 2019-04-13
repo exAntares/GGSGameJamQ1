be7b150d314bf0452d484102b239814230fd88c2
exAntares <luis.andres81@gmail.com>
2019-04-13 12:05:19 +0200

    added attack animation to hero and new scenes

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    private HeroController _player;
+
+    private void Awake() {
+        _player = FindObjectOfType<HeroController>();
+    }
+
+    private void FixedUpdate() {
+        
+    }
+}
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using HalfBlind.ScriptableVariables;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    [SerializeField] private EnemyController[] _enemyPrefabs;
+    [SerializeField] private GlobalFloat _currentLevel;
+
+    private void Awake() {
+        for (int i = 0; i < _currentLevel.Value + 1; i++) {
+            for (int j = 0; j < _enemyPrefabs.Length; j++) {
+                Instantiate(_enemyPrefabs[j]);
+            }
+        }
+    }
+}

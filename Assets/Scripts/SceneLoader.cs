0833a1ad83055142b93582d16f28a59116562b70
exAntares <luis.andres81@gmail.com>
2019-04-13 18:14:39 +0200

    Added scene loader to train

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+}

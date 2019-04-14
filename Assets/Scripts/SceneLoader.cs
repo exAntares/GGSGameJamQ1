cb98bf8f8af2265b378ef7b3376ec56f26334bb3
exAntares <luis.andres81@gmail.com>
2019-04-14 14:16:55 +0200

    Added background to game

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,5 +5,6 @@ public class SceneLoader : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
     }
 }
